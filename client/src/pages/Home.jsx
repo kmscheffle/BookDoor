9fc9febc9900bbed57ef60a56f5327676aa4af21
Hogward <jaegern23@gmail.com>
2024-02-27 20:43:20 -0600

    add BookForm styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
         >
           <BookForm />
         </div>
